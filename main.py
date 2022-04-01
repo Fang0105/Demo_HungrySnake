5e3f1c2add440d2303ee18a89136d6112261fbde
Fang0105 <nody0105@gmail.com>
2022-04-01 09:31:41 +0800

    add txt[time,score,rank] in rects

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame as pg
-import setting
 import setting as st
 import sys
 from pygame.locals import QUIT
@@ -13,10 +12,16 @@ wd = pg.display.set_mode(st.windowsSize)
 #設定Surface
 sf = pg.Surface(wd.get_size())
 sf.blit(st.imageBackGround,st.imageBackGroundLocation)
-
 wd.blit(sf,(0,0))
 
+pg.draw.rect(sf,(255,0,0),st.rectTime)
+pg.draw.rect(sf,(255,0,0),st.rectScore)
+pg.draw.rect(sf,(255,0,0),st.rectRank)
+sf.blit(st.txtTime,st.txtTime.get_rect(center=st.rectTime.center))
+sf.blit(st.txtScore,st.txtScore.get_rect(center=st.rectScore.center))
+sf.blit(st.txtRank,st.txtRank.get_rect(center=st.rectRank.center))
 
+wd.blit(sf,(0,0))
 
 pg.display.update()
 
