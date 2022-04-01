84f9cc6f88f0af2e0e1868742a1cc7862c133389
Fang0105 <nody0105@gmail.com>
2022-04-01 14:48:57 +0800

    add random apple location

--- setting.py
+++ setting.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame as pg
 
 pg.init()
@@ -6,6 +8,7 @@ gameName='Hungry Snake'
 
 imageBackGround = pg.image.load('background.png')
 imageBackGroundLocation = (222.5,0)
+latticeSize = 777.5/16
 
 font = pg.font.Font(None,70)
 txtTime = font.render("TIME",True,(255,255,0))
@@ -14,3 +17,11 @@ txtScore = font.render("SCORE",True,(255,255,0))
 rectScore = pg.Rect(0,200,222.5,50)
 txtRank = font.render("Rank",True,(255,255,0))
 rectRank = pg.Rect(0,400,222.5,50)
+
+imageApple = pg.image.load('apple.png')
+imageApple = pg.transform.scale(imageApple,(latticeSize,latticeSize))
+imageAppleLocation = ""
+
+def getRandomAppleLocation():
+    imageAppleLocation = (222.5 + latticeSize * random.randint(0, 15), 0 + latticeSize * random.randint(0, 15))
+    return imageAppleLocation
