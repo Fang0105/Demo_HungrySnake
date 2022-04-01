fc0d0417f884c9427d6cc749ec925b5acb7948c9
Fang0105 <nody0105@gmail.com>
2022-04-01 09:32:31 +0800

    add txt[time,score,rank] and rect[time,score,rank]

--- setting.py
+++ setting.py
@@ -1,9 +1,16 @@
 import pygame as pg
 
+pg.init()
 windowsSize=(1000,777.5)
 gameName='Hungry Snake'
+
 imageBackGround = pg.image.load('background.png')
-imageBackGroundLocation = (223,0)
-scoreLabel = "SCORE"
-timeLabel = "TIME"
-rankLabel = "RANK"
+imageBackGroundLocation = (222.5,0)
+
+font = pg.font.Font(None,70)
+txtTime = font.render("TIME",True,(255,255,0))
+rectTime = pg.Rect(0,0,222.5,50)
+txtScore = font.render("SCORE",True,(255,255,0))
+rectScore = pg.Rect(0,200,222.5,50)
+txtRank = font.render("Rank",True,(255,255,0))
+rectRank = pg.Rect(0,400,222.5,50)
