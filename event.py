5fa71a59e4886fb8c51c751806d45db083bdc883
Fang0105 <nody0105@gmail.com>
2022-04-07 15:32:39 +0800

    make snake head rotate

--- event.py
+++ event.py
@@ -1,10 +1,52 @@
 import pygame as pg
 import sys
+import setting as st
+from pygame import *
 from pygame.locals import QUIT
+import game
+import apple
 
-def event():
+def event(wd,sf):
     while True:
         for ev in pg.event.get():
             if ev.type == QUIT:
                 pg.quit()
                 sys.exit()
+            elif ev.type == KEYDOWN:
+                if ev.key == K_RIGHT:
+                    if st.direction == "left":
+                        pass
+                    else:
+                        if st.direction == "up":
+                            st.rotateSnakeHead(270,game.listSnake,sf,wd)
+                        elif st.direction == "down":
+                            st.rotateSnakeHead(90,game.listSnake,sf,wd)
+                        st.direction = "right"
+                elif ev.key == K_LEFT:
+                    if st.direction == "right":
+                        pass
+                    else:
+                        if st.direction == "up":
+                            st.rotateSnakeHead(90,game.listSnake,sf,wd)
+                        elif st.direction == "down":
+                            st.rotateSnakeHead(270,game.listSnake,sf,wd)
+                        st.direction = "left"
+                elif ev.key == K_UP:
+                    if st.direction == "down":
+                        pass
+                    else:
+                        if st.direction == "right":
+                            st.rotateSnakeHead(90,game.listSnake,sf,wd)
+                        elif st.direction == "left":
+                            st.rotateSnakeHead(270,game.listSnake,sf,wd)
+                        st.direction = "up"
+                elif ev.key == K_DOWN:
+                    if st.direction == "up":
+                        pass
+                    else:
+                        if st.direction == "right":
+                            st.rotateSnakeHead(270,game.listSnake,sf,wd)
+                        elif st.direction == "left":
+                            st.rotateSnakeHead(90,game.listSnake,sf,wd)
+                        st.direction = "down"
+                print(st.direction)
