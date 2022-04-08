512e3b4712673adf793fbb9ce256b4a5d2926702
Fang0105 <nody0105@gmail.com>
2022-04-08 23:53:26 +0800

    make snakeHead move!!!!!!

--- snake.py
+++ snake.py
@@ -1,4 +1,8 @@
 import setting as st
+import abc
+import apple
+import frame
+import pygame as pg
 
 class Snake:
     location = (None,None)
@@ -6,14 +10,56 @@ class Snake:
     def __init__(self,location):
         self.location = location
 
+    @abc.abstractmethod
+    def move(self):
+        return NotImplemented
+
 class SnakeHead(Snake):
     direction = None
     image = st.imageSnakeHead
     def __init__(self,location,direction):
         super().__init__(location)
         self.direction = direction
-
+    def isCollision(self):
+       global boolIsCollision
+       boolIsCollision = None
+       if self.location[0]>15 or self.location[1]>15 or self.location[0]<0 or self.location[1]<0:
+           boolIsCollision = True
+       else:
+           if self.location in st.setLocation:
+               if self.location == st.imageAppleLocation:
+                   print("hit apple")
+                   apple.generateNewApple(frame.sf,frame.wd)
+                   boolIsCollision =  False
+               else:
+                   print("hit body")
+                   boolIsCollision =  True
+           else:
+               boolIsCollision = False
+       if boolIsCollision == False:
+           return boolIsCollision
+       else:
+           st.gameStop = True
+           return boolIsCollision
+    def move(self):
+        tmp = self.location
+        st.setLocation.remove(tmp)
+        if self.direction == "right":
+            self.location = (self.location[0]+1,self.location[1])
+        elif self.direction == "left":
+            self.location = (self.location[0]-1,self.location[1])
+        elif self.direction == "up":
+            self.location = (self.location[0],self.location[1]-1)
+        elif self.direction == "down":
+            self.location = (self.location[0],self.location[1]+1)
+        if self.isCollision():
+            print(st.gameStop)
+            self.location = tmp
+        else:
+            st.setLocation.add(self.location)
 class SnakeBody(Snake):
     image = st.imageSnakeBody
     def __init__(self,location):
         super().__init__(location)
+    def move(self):
+        pass
