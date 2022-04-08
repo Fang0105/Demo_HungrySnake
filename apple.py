f1a81986ee4c19ff1f0fa917e53bebe97cc88d37
Fang0105 <nody0105@gmail.com>
2022-04-08 23:51:34 +0800

    add [drawApple function] and [generateNewApple function]

--- apple.py
+++ apple.py
@@ -1,8 +1,24 @@
 import setting as st
 import random
+import pygame as pg
 
+def getRandomAppleCoordinate():
+    st.imageAppleLocation = (random.randint(0, 15),random.randint(0, 15))
+    while st.imageAppleLocation in st.setLocation == True:
+        st.imageAppleLocation = (random.randint(0, 15), random.randint(0, 15))
+    st.setLocation.add(st.imageAppleLocation)
+    return st.transformLocationToCoordinate(st.imageAppleLocation)
 
+def drawApple(sf,wd):
 
-def getRandomAppleLocation():
-    st.imageAppleLocation = (st.transformCoordinateTolocation(random.randint(0, 15),random.randint(0, 15)))
-    return st.imageAppleLocation
+    sf.blit(st.imageBackGround, st.imageBackGroundLocation)
+    sf.blit(st.imageApple, getRandomAppleCoordinate())
+    wd.blit(sf, (0, 0))
+
+def generateNewApple(sf,wd):
+    st.setLocation.remove(st.imageAppleLocation)
+    sf.blit(st.imageBackGround, st.imageBackGroundLocation)
+    sf.blit(st.imageApple, getRandomAppleCoordinate())
+    wd.blit(sf, (0, 0))
+    st.setLocation.add(st.imageAppleLocation)
+    pg.display.update()
