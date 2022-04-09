464f9d68b0bd27b39b4e825ae3ad8b8128626ab9
Fang0105 <nody0105@gmail.com>
2022-04-09 22:25:32 +0800

    debug : apple new location appeared at snake location         (solved)

--- apple.py
+++ apple.py
@@ -1,24 +1,23 @@
 import setting as st
+import frame
 import random
 import pygame as pg
 
 def getRandomAppleCoordinate():
     st.imageAppleLocation = (random.randint(0, 15),random.randint(0, 15))
-    while st.imageAppleLocation in st.setLocation == True:
+    while st.imageAppleLocation in st.setLocation:
         st.imageAppleLocation = (random.randint(0, 15), random.randint(0, 15))
     st.setLocation.add(st.imageAppleLocation)
     return st.transformLocationToCoordinate(st.imageAppleLocation)
 
-def drawApple(sf,wd):
+def drawApple():
+    frame.sf.blit(st.imageApple, getRandomAppleCoordinate())
 
-    sf.blit(st.imageBackGround, st.imageBackGroundLocation)
-    sf.blit(st.imageApple, getRandomAppleCoordinate())
-    wd.blit(sf, (0, 0))
+def generateNewApple():
 
-def generateNewApple(sf,wd):
     st.setLocation.remove(st.imageAppleLocation)
-    sf.blit(st.imageBackGround, st.imageBackGroundLocation)
-    sf.blit(st.imageApple, getRandomAppleCoordinate())
-    wd.blit(sf, (0, 0))
+    frame.sf.blit(st.imageBackGround, st.imageBackGroundLocation)
+    frame.sf.blit(st.imageApple, getRandomAppleCoordinate())
+    frame.wd.blit(frame.sf, (0, 0))
     st.setLocation.add(st.imageAppleLocation)
     pg.display.update()
