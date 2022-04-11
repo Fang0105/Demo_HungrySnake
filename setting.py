278671a6b86198f3ec994d70e647d180413056ef
Fang0105 <nody0105@gmail.com>
2022-04-11 23:15:02 +0800

    add functions about clock

--- setting.py
+++ setting.py
@@ -26,7 +26,7 @@ def transformLocationToCoordinate(location):
     return location
 
 def drawScore(sf,wd):
-    fontScore = pg.font.Font(None, 200)
+    fontScore = pg.font.Font(None, 190)
     txtScore = fontScore.render(str(score), True, (255, 255, 0))
     rectScore = pg.Rect(0, 250, 222.5, 150)
     pg.draw.rect(sf, (255, 0, 255), rectScore)
@@ -57,4 +57,30 @@ shouldGenerateNewApple = False
 gameStop = False
 
 hasMoved = False
+
+minute = 0
+second = 0
+def getClock():
+    strSecond = str(second)
+    strMinute = str(minute)
+    if second<10:
+        strSecond = "0"+strSecond
+    if minute<10:
+        strMinute = "0"+strMinute
+    return (strMinute+":"+strSecond)
+
+def addOneSecond():
+    global second
+    global minute
+    second += 1
+    if second == 60:
+        second = 0
+        minute += 1
+def drawClock(sf):
+    fontClock = pg.font.Font(None, 120)
+    txtClock = fontClock.render(getClock(), True, (255, 255, 0))
+    rectClock = pg.Rect(0, 50, 222.5, 150)
+    pg.draw.rect(sf, (255, 0, 255), rectClock)
+    sf.blit(txtClock,txtClock.get_rect(center=rectClock.center))
+
 
