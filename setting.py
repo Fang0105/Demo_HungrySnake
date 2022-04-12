233e1dac89d8d38c80bd0d4e02cdcb65737054cd
Fang0105 <nody0105@gmail.com>
2022-04-12 23:21:40 +0800

    add drawRank function

--- setting.py
+++ setting.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import pandas as pd
 
 pg.init()
 windowsSize=(1000,777.5)
@@ -18,7 +19,7 @@ rectTimeText = pg.Rect(0,0,222.5,50)
 txtLabelScore = font.render("SCORE",True,(255,255,0))
 rectScoreText = pg.Rect(0,200,222.5,50)
 
-txtLabelRank = font.render("Rank",True,(255,255,0))
+txtLabelRank = font.render("RANK",True,(255,255,0))
 rectRankText = pg.Rect(0,400,222.5,50)
 
 def transformLocationToCoordinate(location):
@@ -83,4 +84,34 @@ def drawClock(sf):
     pg.draw.rect(sf, (255, 0, 255), rectClock)
     sf.blit(txtClock,txtClock.get_rect(center=rectClock.center))
 
-
+def drawRank(sf):
+    record = pd.read_csv("record.csv")
+    fontRank = pg.font.Font(None,30)
+    txtRankRank = fontRank.render("rank",True,(0,0,0))
+    rectRankRank = pg.Rect(0,450,50,27.5)
+    pg.draw.rect(sf,(0,255,0),rectRankRank)
+    sf.blit(txtRankRank,txtRankRank.get_rect(center=rectRankRank.center))
+    txtRankScore = fontRank.render("score",True,(0,0,0))
+    rectRankScore = pg.Rect(50, 450, 80, 27.5)
+    pg.draw.rect(sf, (255, 255, 255), rectRankScore)
+    sf.blit(txtRankScore, txtRankScore.get_rect(center=rectRankScore.center))
+    txtRankTime = fontRank.render("time", True, (0, 0, 0))
+    rectRankTime = pg.Rect(130, 450, 92.5, 27.5)
+    pg.draw.rect(sf, (0, 255, 255), rectRankTime)
+    sf.blit(txtRankTime, txtRankTime.get_rect(center=rectRankTime.center))
+    for i in range(0,6):
+        rank = str(i+1)
+        score = str(record.at[i,"score"])
+        time = record.at[i,"time"]
+        txtRecordRank = fontRank.render(rank, True, (0, 0, 0))
+        rectRecordRank = pg.Rect(0, 477.5+50*i, 50, 50)
+        pg.draw.rect(sf, (250, 255, 0), rectRecordRank)
+        sf.blit(txtRecordRank, txtRecordRank.get_rect(center=rectRecordRank.center))
+        txtRecordScore = fontRank.render(score, True, (0, 0, 0))
+        rectRecordScore = pg.Rect(50, 477.5+50*i, 80, 50)
+        pg.draw.rect(sf, (250, 0, 255), rectRecordScore)
+        sf.blit(txtRecordScore, txtRecordScore.get_rect(center=rectRecordScore.center))
+        txtRecordTime = fontRank.render(time, True, (0, 0, 0))
+        rectRecordTime = pg.Rect(130, 477.5+50*i, 92.5, 50)
+        pg.draw.rect(sf, (255, 0, 0), rectRecordTime)
+        sf.blit(txtRecordTime, txtRecordTime.get_rect(center=rectRecordTime.center))
